6196c73cee7b6a8ae880f6d949aa81f266c6badc
newparadigma1 <newparadigma1@gmail.com>
2018-01-16 18:01:17 +0300

    first work game

--- game.js
+++ game.js
@@ -1,3 +1,4 @@
+
 function game() {
   var array =
   [
@@ -7,67 +8,88 @@ function game() {
     'blue',
     'purple',
   ];
-  var current_color = array[randomInteger(0, 4)]
+
+  var current_color;
   var score = 0;
   var interval = 5000;
+  var interval_id;
 
   var buttons_block = document.getElementById('buttons');
   var current_color_el = document.getElementById('current_color');
   var score_block =document.getElementById('score');
-
-  document.getElementsByClassName('button').onclick = press_button;
+  var interval_el =document.getElementById('interval');
 
   score_block.innerText = score;
-
-  current_color_el.innerText = "COLOR";
-  // var index = randomInteger(0, 4);
-  place_buttons(array);
+  interval_el.innerText = interval;
+  change_current_color();
+  replace_buttons();
   runtime();
 
+  function runtime() {
+    interval_id =
+    setInterval(function() {
+      change_current_color();
+      replace_buttons();
+    }, interval);
+  }
+
+  function change_current_color() {
+    array_length = array.length;
+    current_color = array[randomInteger(0, array_length - 1)];
+    current_color_el.className = current_color;
+  }
+
+  function replace_buttons() {
+    //init
+    var button_texts = shuffle(array);
+    // var button_colors = shuffle(button_texts);
+
+    console.log(button_texts);
+    console.log(button_colors);
+    //clear area
+    buttons_block.innerHTML = "";
+    //replace_buttons
+    for (var i = 0; i < array.length; i++) {
+      var button = document.createElement('button');
+      button.id = i;
+      button.innerText = button_texts[i];
+      button.className = button_texts[randomInteger(0, button_texts.length - 1)];
+      button.addEventListener("click", press_button);
+      buttons_block.appendChild(button);
+    }
+  }
+
+  function press_button() {
+    if (this.innerText == current_color) {
+      score++;
+      interval -= 100;
+
+      clearInterval(interval_id);
+      score_block.innerText = score;
+      interval_el.innerText = interval;
+
+      change_current_color();
+      replace_buttons();
+      runtime();
+    } else {
+      buttons_block.innerHTML = "";
+      clearInterval(interval_id);
+    }
+  }
+
+  //---------------------
   function randomInteger(min, max) {
-    var rand = min - 0.5 + Math.random() * (max - min + 1)
+    var rand = min - 0.5 + Math.random() * (max - min + 1);
     rand = Math.round(rand);
     return rand;
   }
 
   function shuffle(a) {
     for (let i = a.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [a[i], a[j]] = [a[j], a[i]];
+      const j = Math.floor(Math.random() * (i + 1));
+      [a[i], a[j]] = [a[j], a[i]];
     }
     return a;
   }
-
-  function place_buttons(colors) {
-    buttons_block.innerHTML = "";
-    colors.forEach(function(currentValue, index, array){
-      var button = document.createElement('button');
-      button.id = index;
-      button.innerText = currentValue;
-      button.addEventListener("click", press_button);
-      buttons_block.appendChild(button);
-    });
-  }
-
-  function press_button() {
-    var currentValue = this.innerText;
-    if (current_color_el.className == currentValue) {
-      score++;
-    } else {
-      score--;
-    }
-    score_block.innerText = score;
-    runtime();
-  }
-
-  function runtime() {
-
-
-    setInterval(function() {
-      current_color_el.className = current_color;
-
-      var colors = shuffle(array);
-      place_buttons(colors);
-    }, interval);
-  }
+  //---------------------
 }
